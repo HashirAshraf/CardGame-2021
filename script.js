01969d02365ede2b63e2a21b74cdaf877bf97995
Max_g <05806@farnborough.ac.uk>
2021-03-24 20:03:25 +0000

    Fixed Deck Shuffle and Array Creation

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ function createCards(cardCol, cardNum) {
     }
     return cardDeck;    //Returns every card in the array
 }
-function shuffleCards(cardDeck) {
-
-    for (let i = 0; i<1000; i++){
-        let index = (Math.floor(Math.random()) * cardDeck.length);
-        let index1 = (Math.floor(Math.random()) * cardDeck.length); //Produces a random integer
-        let temp = cardDeck[index];
-        cardDeck[index] = cardDeck[index1];
-        cardDeck[index1] = temp;    //Changes the value of the position of the cards in the deck
+function shuffleCards(cardDeck)
+{
+    for (let i = 0; i < 1000; i++) {
+        let index1 = Math.floor((Math.random() *  cardDeck.length));
+        let index2 = Math.floor((Math.random() *  cardDeck.length));
+        let temp = cardDeck[index1];
+        cardDeck[index1] = cardDeck[index2];
+        cardDeck[index2] = temp;
     }
 }
 
@@ -37,39 +37,39 @@ function displayCards(cardDeck, cardNum) {
 
 
 function pickCards(shuffledDeck) {
-    let playerOneDeck = [];
-    let playerTwoDeck = [];
-    let i = 1
-    while (i < shuffledDeck.length) {
+    let playerOneDeck = []; //Creates an Array for Player One's deck
+    let playerTwoDeck = []; //Creates an Array for Player Two's deck
+    let i = 1   //Creates an index
+    while (i < shuffledDeck.length) {   //While loop to shuffle give cards to each Player's hand
         if (shuffledDeck[i - 1].Suit == shuffledDeck[i].Suit) {
             if (shuffledDeck[i - 1].Value > shuffledDeck[i].Value) {
-                playerOneDeck.push({Suit: shuffledDeck[i - 1].Suit, Value: shuffledDeck[i - 1].Value});
+                playerOneDeck.push({Suit: shuffledDeck[i - 1].Suit, Value: shuffledDeck[i - 1].Value}); //Pushes a random card from the shuffled deck to Player One's deck
                 playerOneDeck.push({Suit: shuffledDeck[i].Suit, Value: shuffledDeck[i].Value});
             }
         } else {
-            playerTwoDeck.push({Suit: shuffledDeck[i - 1].Suit, Value: shuffledDeck[i - 1].Value});
+            playerTwoDeck.push({Suit: shuffledDeck[i - 1].Suit, Value: shuffledDeck[i - 1].Value}); //Pushes a random card from the shuffled deck to Player Two's deck
             playerTwoDeck.push({Suit: shuffledDeck[i].Suit, Value: shuffledDeck[i].Value});
         }
     }
     if (playerOneDeck.length > playerTwoDeck.length) {
-        displayDeck("Player One", playerOneDeck);
+        displayDeck("Player One", playerOneDeck);   //Shows Player One's Deck if they have more cards
     }
     else{
-            displayDeck("Player Two", playerTwoDeck);
+            displayDeck("Player Two", playerTwoDeck);   //Shows Player Two's Deck if they have more cards
         }
 }
 
 function displayDeck(winner, deck) {
-    document.getElementById("winningPlayer").innerHTML = winner;
-    document.getElementById("noOfCards").innerHTML = deck.length;
+    document.getElementById("winningPlayer").innerHTML = winner;    //Receives the Winner of the match
+    document.getElementById("noOfCards").innerHTML = deck.length;   //Receives the number of cards the Winner had at the end of the match
 
 
-    let deckHTML = '<table><tr>'
+    let deckHTML = '<table><tr>';
 
     for (let i = deck.length - 1; i < 0; i++) {
         let card = deck[i].Suit + deck[i].Value;
         let image = "img/" + deck[i].Suit + deck[i].Value + ".jpg";
-        deckHTML += `<td>${card}</td><td><img src=${image} width="40px" height="60px"></td>`;
+        deckHTML += `<td>${card}</td><td><img src=${image} width="40px" height="60px"></td>`;   //Creates an image file for each card
         deckHTML += '</tr></table>';
         document.getElementById("winner").innerHTML = deckHTML;
     }
